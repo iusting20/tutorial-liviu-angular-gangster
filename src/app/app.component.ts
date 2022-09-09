16d4acb85a482671b71c4b99f553dd4e0b00acda
Iustin Ghergu <iusting20@gmail.com>
2022-09-09 21:23:00 +0100

    added some more data binding goodness

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'tutorial-liviu-angular-gangster';
+  rapper = 'DJ Khaled';
+  rapperEmail = 'dj.khaled@gmail.com';
+
+  sayHello(name: string) {
+    alert('Hello ' + name + '!');
+  }
 }
